336683e38dde174ecd08facef2c69b31a8a187ed
NAZLI <nazlikoymat@karatekin.edu.tr>
2023-12-04 16:54:51 +0300

    güncelleme2

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
-import { Box, Divider, Chip, Grid, Paper, Typography,Card,CardContent,CardActions,Button } from "@mui/material";
+import { Box, Divider, Chip, Grid, Paper, Typography,Button } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,36 +13,117 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function cardComponents({ data }){
   var mdx;
+  var heightx;
+  var widthx;
   if(data.id==1){
-    mdx=7;
+    mdx=12;
+    heightx="500px";
+    widthx="1200px"
+    
   }
-  else if(data.id==2){
-  mdx=5;}
+
   else{
-  mdx=4;
+  mdx=6;
+  heightx="300px";
+  widthx="400px"
 }
 console.log(mdx)
     return(     
-      <Grid item md={mdx} xs={mdx}>
-      <Item style={{height:"400px",backgroundImage: `url(https://img.freepik.com/free-vector/3d-earth-graphic-symbolizing-global-trade-illustration_456031-131.jpg?w=1060&t=st=1701632662~exp=1701633262~hmac=a1006e57b01f921a804632c447634e73f7c445d7909d99996b442f103acc5c6b)`,backgroundSize:"cover"}}> 
-               <Card sx={{background:'none',color:"white"}}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                  {data.name  }{ data.username}
-                  </Typography>
-                
-                   <Typography variant="body2">
-                     {data.name}
-                      </Typography>
-                </CardContent>
-                <CardActions style={{marginBottom:0}}>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-      </Item>
-   </Grid> 
-     
+      <Grid item xs={12} md={4}>
+      <Paper sx={{ borderRadius: "15px", backgroundColor: "#35356b" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            padding: "20px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              marginTop: "10px",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <img
+              src="https://e7.pngegg.com/pngimages/24/817/png-clipart-playstation-4-raiders-of-the-broken-planet-playstation-network-playstation-plus-playstation-electronics-text.png"
+              alt="cardimage"
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            />
+            <Typography variant="p">{data.name}</Typography>
+          </Box>
+
+          <Divider sx={{ width: "99%", mt: 1 }} />
+          <Typography variant="p" sx={{ mt: 1 }}>
+            {data.name}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              marginTop: "10px",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Chip label={data.chip} color={data.chipColor} />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                ml: 1,
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://www.svgrepo.com/show/13695/star.svg"
+                alt="cardimage"
+                style={{ width: "20px", height: "20px", marginRight: "5px" }}
+              />
+              <Typography variant="h6">{data.name}</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                ml: 1,
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb0Hmlxrb-nSWkZtxnUJgW9fyLgiSf_Re9tw&usqp=CAU"
+                alt="cardimage"
+                style={{ width: "20px", height: "20px", marginRight: "5px" }}
+              />
+              <Typography variant="h6">{data.name}</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                ml: 1,
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcEWGtqr8nQtm3GSnkk3QCL2CwQou1zr4FiA&usqp=CAU"
+                alt="cardimage"
+                style={{ width: "20px", height: "20px", marginRight: "5px" }}
+              />
+              <Typography variant="h6">{data.name}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
      );
     }
 cardComponents.prototype={
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,11 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import CardComponents from '../component/card';
+import { Box, Divider, Chip, Grid, Paper, Typography,Button } from "@mui/material";
 
 
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
 
-}));
 
 export default function HomeScreen()
 { 
@@ -32,17 +22,175 @@ export default function HomeScreen()
 
   }, []);
 
-     return(     
-      <Box  sx={{ flexGrow: 1,background:"#e9e9e9",m:4,p:5}}>
-            <Grid container spacing={2}>  
-            {data.map((item,index) => (    
-              
-            <CardComponents  key={index} data={item}/>
-        ))}
-        
+     return(  
+      <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height:"1050px"
+      }}
+    >
+      <Grid container spacing={3}  xs={12} md={12} sx={{width:"100%"}} >     
+        <Grid item xs={12} md={8} >
+          <Box sx={{
+                  borderRadius: "15px",
+                  backgroundColor: "#35356b",
+                  backgroundImage:`url(${"https://images.unsplash.com/photo-1585592377048-a627ec452b7e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyc3x8fHx8fDE3MDE2OTczNDE&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"})`,
+                  backgroundSize:"cover",
+                  height:"600px",
+                  width:"100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-end",
+                  alignItems:"flex-start",marginBottom:"10px"
+                  }}>
+                   <Typography variant='h2'
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "flex-start",
+                      alignItems: "center",
+                      margin:"30px",
+                      color:"white"
+                    }}
+                   >
+                içerikkk
+              </Typography>            
+          </Box>
+           <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent:"space-between",
+                width:"100%"
+                }}
+            >     
+              <Grid  item xs={12} md={6} sx={{padding:"5px" }}>
+                  <Box sx={{
+                    borderRadius: "15px",
+                    backgroundColor: "#35356b",
+                    backgroundImage:`url(${"https://images.unsplash.com/photo-1585592377048-a627ec452b7e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyc3x8fHx8fDE3MDE2OTczNDE&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"})`,
+                    backgroundSize:"cover",
+                    width:"100%",
+                    height:"420px",
+                    }}>
+                            
+                      <Typography variant='h2'
+                        sx={{
+                          display: "flex",
+                          flexDirection:"column",
+                          justifyContent:"flex-end",
+                          alignItems:"flex-start",
+                          width:"90%",  
+                          height:"400px",
+                          color:"white",
+                          marginLeft:"20px"                
+                        }}
+                      >
+                        içerikkkss sol
+                  
+                    </Typography>
+                  </Box>
+              </Grid>
+              <Grid item xs={12} md={6} sx={{padding:"5px" }}>
+                  <Box sx={{
+                      borderRadius: "15px",
+                      backgroundColor: "#35356b",
+                      backgroundImage:`url(${"https://images.unsplash.com/photo-1585592377048-a627ec452b7e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyc3x8fHx8fDE3MDE2OTczNDE&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"})`,
+                      backgroundSize:"cover",
+                      width:"100%",
+                      height:"420px",
+                    }}>
+                  
+                  <Typography variant='h2'
+                        sx={{
+                          display: "flex",
+                          flexDirection:"column",
+                          justifyContent:"flex-end",
+                          alignItems:"flex-start",
+                          width:"90%",  
+                          height:"400px",
+                          color:"white",
+                          marginLeft:"20px"                
+                        }}
+                      >
+                        içerikkkss sağ
+                  
+                    </Typography>
+                  </Box>
+              </Grid>
+           </Box>
         </Grid>
-      </Box>
-
+        <Grid item xs={12}  md={4} >
+          <Box sx={{
+             display: "flex",
+             flexDirection: "column",
+             justifyContent:"flex-start",
+             alignItems:"flex-start",
+             width:"100%",height:"100%",
+            paddingBottom:"10px"}}>
+                <Box sx={{
+                  width:"100%",
+                  backgroundImage:`url(${"https://images.unsplash.com/photo-1585592377048-a627ec452b7e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyc3x8fHx8fDE3MDE2OTczNDE&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"})`,
+                  backgroundSize:"cover",
+                  height:"50%",                
+                  borderRadius: "15px",
+                  display: "flex",
+                  flexDirection:"column",
+                  justifyContent:"flex-end",
+                  alignItems:"flex-start",
+                  color:"white" , marginBottom:"20px"  
+                }}
+                  >  
+                    <Typography variant='h2'
+                  sx={{
+                    display: "flex",
+                    flexDirection:"column",
+                    justifyContent:"flex-end",
+                    alignItems:"flex-start",
+                    width:"90%",  
+                    height:"400px",
+                    color:"white",
+                    marginBottom:"20px"  ,
+                    marginLeft:"10px"                 
+                  }}
+                >
+                    fdf</Typography> 
+                    
+                    </Box>
+                    <Box sx={{
+                  width:"100%",
+                  backgroundImage:`url(${"https://img.freepik.com/free-photo/global-connections_53876-89039.jpg?w=1380&t=st=1701687394~exp=1701687994~hmac=68503d3719a8658b1730851f4f78c828027f415c7f02f5e1b9189e7ae1ca4962"})`,
+                  backgroundSize:"cover",
+                  height:"50%",                
+                  borderRadius: "15px",
+                  display: "flex",
+                  flexDirection:"column",
+                  justifyContent:"flex-end",
+                  alignItems:"flex-start",
+                  color:"white" }}
+                  >
+                       <Typography 
+                  sx={{
+                    display: "flex",
+                    flexDirection:"column",
+                    justifyContent:"flex-end",
+                    alignItems:"flex-start",
+                    width:"90%",  
+                    height:"400px",
+                    color:"white",
+                    marginBottom:"20px",
+                    marginLeft:"10px"                  
+                  }}
+                >fdf</Typography>
+                    </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
   
